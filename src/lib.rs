8e13cc2cf46f5fb0edad6ec85ecc84c1f124be7f
Tom Wolcott <wolcott.tomj@gmail.com>
2023-10-10 21:12:27 -0400

    added debug options

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,20 @@ pub fn template(input: TokenStream) -> TokenStream {
     let mut rust_expressions: Vec<String> = Vec::new();
     let mut template = input.to_string();
 
-    let regex = Regex::new(r"\{\{|\}\}|\{|\}").unwrap();
+    let regex = Regex::new(r"\{\{|\}\}|\{|\(:[^)]+\)\}|\}").unwrap();
     let mut paren_level = 0;
     let mut expr_ranges = Vec::new();
 
     for capture in regex.captures_iter(template.as_str()) {
-        let (str, []) = capture.extract::<0>();
+        let (mut str, []) = capture.extract::<0>();
 
         match str {
             "{" => {
                 if paren_level == 0 {
                     expr_ranges.push((
                         capture.get(0).unwrap().end(),
-                        capture.get(0).unwrap().end()
+                        capture.get(0).unwrap().end(),
+                        capture.get(0).unwrap().end(),
                     ))
                 }
 
@@ -30,9 +31,18 @@ pub fn template(input: TokenStream) -> TokenStream {
 
                 if paren_level == 0 {
                     expr_ranges.last_mut().unwrap().1 = capture.get(0).unwrap().start();
+                    expr_ranges.last_mut().unwrap().2 = capture.get(0).unwrap().start();
                 }
             },
-            _ => {}
+            "{{" | "}}" => {},
+            formatter => {
+                paren_level -= 1;
+
+                if paren_level == 0 {
+                    expr_ranges.last_mut().unwrap().1 = capture.get(0).unwrap().start();
+                    expr_ranges.last_mut().unwrap().2 = capture.get(0).unwrap().start() + formatter.len();
+                }
+            }
         };
     }
 
@@ -40,9 +50,14 @@ pub fn template(input: TokenStream) -> TokenStream {
         panic!("{}", "Found unmatched curly braces, make sure if you have nested braces you space them out: \"{if x {1} else {2}}\" -> \"{ if x {1} else {2} }\"")
     }
 
-    for &(start, end) in expr_ranges.iter().rev() {
-        rust_expressions.push(template[start..end].to_string());
-        template = format!("{}{}", &template[..start], &template[end..]);
+    for &(start, end1, end2) in expr_ranges.iter().rev() {
+        rust_expressions.push(template[start..end1].to_string());
+
+        template = if end1 == end2 {
+            format!("{}{}", &template[..start], &template[end1..])
+        } else {
+            format!("{}{}{}", &template[..start], &template[end1+1..end2-2], &template[end2-1..])
+        }
     }
 
     rust_expressions = rust_expressions.into_iter().rev().collect();
--- tests/test.rs
+++ tests/test.rs
@@ -6,5 +6,9 @@ use basic_templater::template;
 fn test() {
     let hello = 4;
 
-    println!("{}", template!("{hello+3}, {2 * hello + 8 - if hello > 2 {3} else {5} }"))
+    println!("{}", template!("{hello+3 (:?)}, {f(2.0)}, {f(2.0) (:?)}, {2 * hello + 8 - if hello > 2 {3} else {5} }"))
+}
+
+fn f(x: f32) -> f32 {
+    x * 2.0
 }
